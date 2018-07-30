9ca74454f92fae073d4630ec7344428fa944e817
mohammad-alfatih <alfaitha@gmail.com>
2018-07-30 11:52:08 -0400

    chore: create game data models & begin creating constants/types

---
+++ src/app/models/game/game-models.ts
@@ -0,0 +1,54 @@
+export namespace GameModels {
+  export class Game {
+    players: Player[];
+    table: Table;
+    turns: Turn[];
+    balls: Ball[];
+    sessionType: number;
+    gameType: number;
+    gameWinner: Player;
+  }
+
+  export class Player {
+    name: string;
+    solidColor: boolean;
+  }
+
+  export class Table {
+    topLeft: Hole;
+    topRight: Hole;
+    middleRight: Hole;
+    bottomRight: Hole;
+    bottomLeft: Hole;
+    middleLeft: Hole;
+  }
+
+  export class Turn {
+    turnNumber: number;
+    player: Player;
+    calledShot: Shot;
+    shotSuccessful: boolean;
+  }
+
+  export class Shot {
+    ball: Ball;
+    hole: Hole;
+    shotResult: number;
+  }
+
+  export class Hole {
+    balls: Ball[];
+  }
+
+  export class Ball {
+    number: number;
+    color: string;
+    solidColor: boolean;
+    drop: Drop;
+  }
+
+  export class Drop {
+    turnNumber: number;
+    accidental: boolean;
+  }
+}
