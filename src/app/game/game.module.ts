12b4788190e29a3004c6d3a1ca34d3eabb059505
mohammad-alfatih <alfaitha@gmail.com>
2018-08-02 09:30:21 -0400

    chore: install @angular/forms

--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { GameRoutingModule } from './game.routing.module';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 import { GameService } from './../providers/game.service';
 
 import { GamePageComponent } from './containers/game-page/game-page.component';
@@ -12,7 +14,10 @@ import { GamePageComponent } from './containers/game-page/game-page.component';
     CommonModule,
     GameRoutingModule
   ],
-  providers: [GameService],
+  providers: [
+    FormBuilder,
+    GameService
+  ],
   declarations: [GamePageComponent]
 })
 export class GameModule { }
